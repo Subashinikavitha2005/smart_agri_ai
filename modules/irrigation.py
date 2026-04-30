0139bb0582f3304a9dedb955a143e6fbdf1b4e65
SUBASHINI M <subashini7496@gmail.com>
2026-04-30 22:33:14 +0530

    Add calculate_irrigation function for water needs

--- modules/irrigation.py
+++ modules/irrigation.py
@@ -1,1 +1,7 @@
-
+def calculate_irrigation(temperature, humidity):
+    if temperature > 30:
+        return "High water needed"
+    elif temperature > 20:
+        return "Medium water needed"
+    else:
+        return "Low water needed"
