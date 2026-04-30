fb5633c3dde7b7110ca9d9cc3d14d2f0c45a39b7
SUBASHINI M <subashini7496@gmail.com>
2026-04-30 22:25:23 +0530

    Add crop recommendation and soil type functions

--- modules/crop_recommend.py
+++ modules/crop_recommend.py
@@ -1,1 +1,10 @@
+def recommend_crops(soil, season, temperature, rainfall):
+    if soil == "Loamy":
+        return ["Wheat", "Rice", "Sugarcane"]
+    elif soil == "Sandy":
+        return ["Millet", "Groundnut", "Cotton"]
+    else:
+        return ["Rice", "Maize", "Pulses"]
 
+def get_soil_types():
+    return ["Loamy", "Sandy", "Clay"]
